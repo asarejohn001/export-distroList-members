587260830b719ffca51bf437dc785a08f18f493b
John Asare <137245223+asarejohn001@users.noreply.github.com>
2024-08-29 14:24:52 +0000

    create a functioin to log all errors, warnings, and success information

--- export-members-from-distroList.ps1
+++ export-members-from-distroList.ps1
@@ -15,3 +15,13 @@ Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline -UserPrincipalName 'enter your UPN'
 
 # Function to log messages
+function Get-Log {
+    param (
+        [string]$LogFilePath,
+        [string]$LogMessage
+    )
+    # Create the log entry with the current date and time
+    $logEntry = "{0} - {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $LogMessage
+    # Append the log entry to the log file
+    Add-Content -Path $LogFilePath -Value $logEntry
+}
